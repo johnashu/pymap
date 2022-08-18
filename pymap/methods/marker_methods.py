e36c82b896737758889acfe908b98051ac2d9067
johnashurst-sg <73337262+johnashurst-sg@users.noreply.github.com>
2022-08-18 20:26:26 +0200

    [REM] unwanted

--- pymap/methods/marker_methods.py
+++ pymap/methods/marker_methods.py
@@ -3,8 +3,6 @@ from pymap.interactive.display import PrintStuff
 from pymap.methods.altas_methods import AtlasMethods
 from pymap.tools.utils import take_input
 
-# from getpass import getpass
-
 
 class MarkerMethods(RunProcess, PrintStuff, AtlasMethods):
 
@@ -21,22 +19,6 @@ class MarkerMethods(RunProcess, PrintStuff, AtlasMethods):
     def set_fields(self, **base_fields) -> None:
         for k, v in base_fields.items():
             setattr(self, k, str(v))
-
-    # def new_validator(self, datadir: str = "") -> None:
-    #     if not datadir:
-    #         datadir = take_input(str, "Enter Directory to save keystore: ")
-    #         pw1, pw2 = "1", "2"
-    #         while 1:
-    #             pw1 = getpass(prompt="Enter Keystore password: ")
-    #             pw2 = getpass(prompt="Re-Enter Keystore password: ")
-    #             if pw1 != pw2:
-    #                 print("Passwords do NOT match, Please try again!")
-    #             else:
-    #                 break
-
-    #     args = ["account", "new"]
-    #     context = {"datadir": datadir}
-    #     self.run_method("", context, args=args, prog="atlas", std_in=f"{pw1}\n{pw2}\n")
 
     def create_account(self) -> None:
         """
