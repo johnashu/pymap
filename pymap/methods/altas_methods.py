20208abd67747518b9f11884acc6b84a649a5567
johnashurst-sg <73337262+johnashurst-sg@users.noreply.github.com>
2022-08-18 21:34:37 +0200

    [UPD] atlas join consensus

--- pymap/methods/altas_methods.py
+++ pymap/methods/altas_methods.py
@@ -6,10 +6,6 @@ class AtlasMethods:
     def __init__(self, **kw) -> None:
         super(AtlasMethods, self).__init__(**kw)
 
-    def set_fields(self, **base_fields) -> None:
-        for k, v in base_fields.items():
-            setattr(self, k, str(v))
-
     def new_validator(self, datadir: str = "") -> None:
         if not datadir:
             datadir = take_input(str, "Enter Directory to save keystore: ")
@@ -25,3 +21,26 @@ class AtlasMethods:
         args = ["account", "new"]
         context = {"datadir": datadir}
         self.run_method("", context, args=args, prog="atlas", std_in=f"{pw1}\n{pw2}\n")
+
+    def join_network(
+        self,
+        datadir: str = "./node",
+        port: int = 30321,
+        miner_validator: str = "0x98efa292822eb7b3045c491e8ae4e82b3b1ac005",
+        unlock: str = "0x98efa292822eb7b3045c491e8ae4e82b3b1ac005",
+        syncmode: str = "full",
+    ) -> None:
+        if not datadir:
+            datadir = take_input(str, "Enter Directory to save keystore: ")
+
+        context = {
+            "datadir": datadir,
+            "syncmode": syncmode,
+            "port": port,
+            "mine --miner.validator": miner_validator,
+            "unlock": unlock,
+        }
+        self.run_method("", context, prog="atlas")
+
+
+# ./atlas --datadir ./node --syncmode "full" --port 30321 --mine --miner.validator 0x98efa292822eb7b3045c491e8ae4e82b3b1ac005 --unlock 0x98efa292822eb7b3045c491e8ae4e82b3b1ac005
