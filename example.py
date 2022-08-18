763a950d2856147b388d23f27094937d4673a417
johnashurst-sg <73337262+johnashurst-sg@users.noreply.github.com>
2022-08-18 20:19:01 +0200

    [UPD] rpc classes

--- example.py
+++ example.py
@@ -2,7 +2,7 @@ from webbrowser import get
 from pymap.methods.marker_methods import MarkerMethods
 from pymap.includes.config import envs, base_fields, rpc_url
 from pymap.interactive.interactive import InteractiveSetup
-from pymap.methods.rpc_methods import get_validators
+from pymap.methods.rpc_methods import RpcMethods
 
 from_env = envs.__dict__
 print(from_env)
@@ -12,5 +12,6 @@ if __name__ == "__main__":
     i = InteractiveSetup(**from_env)
     # i.start()'
 
-    res = get_validators(endpoint=rpc_url)
+    rpc = RpcMethods()
+    res = rpc.get_validators(endpoint=rpc_url)
     print(res)
