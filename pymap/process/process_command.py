53c6abc63dd622869c562c5c67d2767d2aca951c
johnashurst-sg <73337262+johnashurst-sg@users.noreply.github.com>
2022-08-19 19:19:14 +0200

    [FIX] key error

--- pymap/process/process_command.py
+++ pymap/process/process_command.py
@@ -49,8 +49,7 @@ class RunProcess:
 
         command_list = [x for x in command_list if x]
 
-        log.info(command_list)
-
+        # log.info(command_list)
         log.info(f'Command to Process:\n\t{" ".join(command_list)}')
 
         try:
