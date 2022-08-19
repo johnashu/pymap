089b946632d84ed4b92fd1d807fa1aac31068b2a
johnashurst-sg <73337262+johnashurst-sg@users.noreply.github.com>
2022-08-19 15:38:50 +0200

    [UPD] error handling

--- example.py
+++ example.py
@@ -8,16 +8,15 @@ from pymap.methods.rpc_methods import RpcMethods
 from_env = envs.__dict__
 print(from_env)
 
-if __name__ == "__main__":
-    m = MarkerMethods(**from_env)
-    # print(dir(MarkerMethods))
-    # i = InteractiveSetup(**from_env)
-    # i.start()
+if __name__ == "__main__":    
 
-    # address = '0xb4e1bc0856f70a55764fd6b3f8dd27f2162108e9'
+    address = '0xb4e1bc0856f70a55764fd6b3f8dd27f2162108e9'
     rpc = RpcMethods()
     res = rpc._get_validators(endpoint=rpc_url)
     print(res)
 
-    # balance = rpc.get_balance(address, endpoint=rpc_url)
-    # print(balance)
+    balance = rpc.get_balance(address, endpoint=rpc_url)
+    print(balance)
+
+    i = InteractiveSetup(**from_env)
+    i.start()
