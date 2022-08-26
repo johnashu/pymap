f7c3fc87ffc481b510f6973dc19e9b766dde3292
Maffaz <johnashu1@hotmail.com>
2022-08-27 00:14:26 +0200

    [ADD] pk from ks

--- pymap/tools/key_from_keystore.py
+++ pymap/tools/key_from_keystore.py
@@ -35,4 +35,4 @@ def pk_from_store(fn: str, password: str) -> None:
     plain_key = dec_suite.decrypt(bytes.fromhex(keystore["crypto"]["ciphertext"]))
     pk = plain_key.hex()
 
-    print(f"\n\tAddress:      {address}\n\tPrivate Key:  {pk}\n")
+    print(f"\n\tAddress:      0x{address}\n\tPrivate Key:  {pk}\n")
