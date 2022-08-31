2d6351337ecc0097696d896e35b40ae405a162c7
Maffaz <johnashu1@hotmail.com>
2022-08-31 12:37:50 +0200

    [FIX] dict service

--- pymap/tools/create_service.py
+++ pymap/tools/create_service.py
@@ -2,15 +2,15 @@ from pymap.tools.file_op import save_file
 import logging as log
 
 
-def create_systemd(
-    context: dict = dict(
-        working_dir="",
-        binary="",
-        password="",
-        datadir="",
-        validator="",
-        unlock="",
-    )
+def create_systemd(    
+    context: dict = {
+        'working_dir': str(),
+        'binary': str(),
+        'password': str(),
+        "datadir": str(),
+        "miner.validator": str(),
+        "unlock": str(),
+        },
 ) -> None:
     template = """
 [Unit]
