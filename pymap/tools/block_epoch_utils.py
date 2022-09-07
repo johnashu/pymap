8017716177fc8d52cafdb749949862545a9b8ce2
johnashurst-sg <73337262+johnashurst-sg@users.noreply.github.com>
2022-09-07 13:43:27 +0200

    [FIX] epoch calculations

--- pymap/tools/block_epoch_utils.py
+++ pymap/tools/block_epoch_utils.py
@@ -14,13 +14,13 @@ def normalise_seconds(seconds: int) -> tuple:
 
 def calc_current_block(current_block, blocks_per_epoch) -> int:
     calc = (current_block - 1) / blocks_per_epoch
-    epoch = floor(calc)
+    epoch = floor(calc) + 1
     return epoch
 
 
 def time_to_next_block(current_block, epoch, blocks_per_epoch) -> datetime:
     block_time = 4.5  # seconds
-    next_epoch = (epoch + 1) * blocks_per_epoch
+    next_epoch = (epoch * blocks_per_epoch) + 1
     blocks_to_next_epoch = next_epoch - current_block
     seconds_to_next_epoch = floor(block_time * blocks_to_next_epoch)
-    return normalise_seconds(seconds_to_next_epoch), next_epoch + 1
+    return normalise_seconds(seconds_to_next_epoch), next_epoch
